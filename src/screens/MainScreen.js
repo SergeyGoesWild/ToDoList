5757f90de36e0702b4fe244cfca5ac3d2e83a10c
Sergey Telnov <sergej.telnov@gmail.com>
2024-03-07 15:25:03 +0100

    Added + button on an overlay, added data states

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,19 +1,49 @@
-import React from "react";
-import { Text, FlatList, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  FlatList,
+  View,
+  StyleSheet,
+} from "react-native";
 import data from "../components/DataMock";
 import OneItem from "../components/OneItem";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function MainScreen() {
-  const Item = ({ item }) => <OneItem itemObj={item} />;
+  const RenderItem = ({ item }) => <OneItem itemObj={item} />;
+  const [itemsData, setItemsData] = useState(data);
+
+  const handleDataChange = (newValue) => {
+    setItemsData(newValue);
+  };
+
+  const handlePlusClick = () => {
+    const newItem = {
+      id: new Date().getTime(),
+      title: "Code!!!",
+      priority: "",
+      description: "Filler text",
+      done: false,
+    };
+    const newValue = [newItem, ...itemsData];
+    handleDataChange(newValue);
+  };
 
   return (
-    <View style={styles.ListContainer}>
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={Item}
-      />
-    </View>
+    <>
+      <View style={styles.ListContainer}>
+        <FlatList
+          data={itemsData}
+          keyExtractor={(item) => item.id}
+          renderItem={RenderItem}
+        />
+      </View>
+
+      <TouchableOpacity onPress={handlePlusClick} style={styles.PlusButton}>
+        <Icon name="plus" size={35} color="white" />
+      </TouchableOpacity>
+    </>
   );
 }
 
@@ -22,6 +52,22 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     marginHorizontal: 30,
+  },
+  PlusButton: {
+    position: "absolute",
+    top: 650,
+    right: 30,
+    backgroundColor: "#ef709d",
+    padding: 15,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
 });
 
