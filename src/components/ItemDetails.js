791e4fe6dcbf3911c0c7a782b3d118e82b28ba05
Sergey Telnov <sergej.telnov@gmail.com>
2024-03-15 12:07:55 +0100

    Refactoring

---
+++ src/components/ItemDetails.js
@@ -0,0 +1,20 @@
+// import React, { useState } from "react";
+// import {
+//   Text,
+//   TextInput,
+//   View,
+//   TouchableWithoutFeedback,
+//   TouchableOpacity,
+// } from "react-native";
+// import { styles } from "../styles/ItemDetails.styles";
+// import BinIcon from "react-native-vector-icons/FontAwesome6";
+// import EditIcon from "react-native-vector-icons/Feather";
+
+// function ItemDetails() {
+
+//   return (
+
+//   );
+// }
+
+// export default ItemDetails;
